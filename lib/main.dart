d037b376e02bfd8da74121d93ff1254aed526328
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-01-05 13:51:41 +0100

    Hide navigation bar and st statusbar transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:rewind_words/home_screen.dart';
+import 'package:rewind_words/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
+    // Hide navigation bar and set status bar transparent
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.transparent,
+    ));
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Rewind Words',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily: 'JandaSafeandSound'
+        fontFamily: 'JandaSafeandSound',
       ),
       home: HomeScreen(),
     );
